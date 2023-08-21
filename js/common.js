c871dd5d5f9c11787a319f418d72814bd4dcfefa
tuhin <csetuhin55@gmail.com>
2023-08-21 14:19:42 +0600

    update commit

---
+++ js/common.js
@@ -0,0 +1,39 @@
+function setCardName(itemName) {
+    number += 1;
+    const setCardNamesElements = document.getElementById('setNames');
+    const p = document.createElement('p');
+    p.innerText = `${number}. ${itemName}
+    `
+    setCardNamesElements.appendChild(p);
+}
+
+function getAmount(id) {
+    const takaElement = document.getElementById(id);
+    const takaElementString = takaElement.innerText;
+    const taka = parseFloat(takaElementString);
+    return taka;
+}
+
+function setTotalPrice(newTotalPrice, id) {
+    const totalPrice = document.getElementById(id);
+    totalPrice.innerText = newTotalPrice.toFixed(2);
+}
+
+function whenApplyEnable(newTotalPrice) {
+    const applyBtn = document.getElementById('applyBtn');
+    if (newTotalPrice >= 200) {
+        applyBtn.removeAttribute('disabled')
+    }
+    else {
+        applyBtn.setAttribute('disabled', true)
+    }
+}
+function whenPurchaseEnable(newTotalPrice) {
+    const purchaseBtn = document.getElementById('purchaseBtn');
+    if (newTotalPrice > 0) {
+        purchaseBtn.removeAttribute('disabled')
+    }
+    else {
+        purchaseBtn.setAttribute('disabled', true)
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,3 +1,111 @@
-function getCardField(){
-    console.log('card added')
+
+let number = 0;
+function handleFirstCard() {
+    const itemElement = document.getElementById('firstCardName');
+    const itemName = itemElement.innerText;
+    setCardName(itemName)
+
+    const firstCardAmount = getAmount('firstCardAmount');
+    const previousTotalPrice = getAmount('totalPrice');
+    const newTotalPrice = previousTotalPrice + firstCardAmount;
+    setTotalPrice(newTotalPrice, 'totalPrice');
+    whenApplyEnable(newTotalPrice);
+    whenPurchaseEnable(newTotalPrice)
 }
+
+function handleSecondCard() {
+    const itemElement = document.getElementById('secondCardName');
+    const itemName = itemElement.innerText;
+    setCardName(itemName)
+
+    const firstCardAmount = getAmount('secondCardAmount');
+    const previousTotalPrice = getAmount('totalPrice');
+    const newTotalPrice = previousTotalPrice + firstCardAmount;
+    setTotalPrice(newTotalPrice, 'totalPrice');
+    whenApplyEnable(newTotalPrice);
+    whenPurchaseEnable(newTotalPrice)
+}
+
+function handleThirdCard() {
+    const itemElement = document.getElementById('thirdCardName');
+    const itemName = itemElement.innerText;
+    setCardName(itemName)
+
+    const firstCardAmount = getAmount('thirdCardAmount');
+    const previousTotalPrice = getAmount('totalPrice');
+    const newTotalPrice = previousTotalPrice + firstCardAmount;
+    setTotalPrice(newTotalPrice, 'totalPrice');
+    whenApplyEnable(newTotalPrice);
+    whenPurchaseEnable(newTotalPrice)
+}
+function handleFourthCard() {
+    const itemElement = document.getElementById('fourthCardName');
+    const itemName = itemElement.innerText;
+    setCardName(itemName)
+
+    const firstCardAmount = getAmount('fourthCardAmount');
+    const previousTotalPrice = getAmount('totalPrice');
+    const newTotalPrice = previousTotalPrice + firstCardAmount;
+    setTotalPrice(newTotalPrice, 'totalPrice');
+    whenApplyEnable(newTotalPrice);
+    whenPurchaseEnable(newTotalPrice)
+}
+function handleFifthCard() {
+    const itemElement = document.getElementById('fifthCardName');
+    const itemName = itemElement.innerText;
+    setCardName(itemName)
+
+    const firstCardAmount = getAmount('fifthCardAmount');
+    const previousTotalPrice = getAmount('totalPrice');
+    const newTotalPrice = previousTotalPrice + firstCardAmount;
+    setTotalPrice(newTotalPrice, 'totalPrice');
+    whenApplyEnable(newTotalPrice);
+    whenPurchaseEnable(newTotalPrice)
+}
+function handleSixthCard() {
+    const itemElement = document.getElementById('sixthCardName');
+    const itemName = itemElement.innerText;
+    setCardName(itemName)
+
+    const firstCardAmount = getAmount('sixthCardAmount');
+    const previousTotalPrice = getAmount('totalPrice');
+    const newTotalPrice = previousTotalPrice + firstCardAmount;
+    setTotalPrice(newTotalPrice, 'totalPrice');
+    whenApplyEnable(newTotalPrice);
+    whenPurchaseEnable(newTotalPrice)
+}
+
+function makePurchaseBtn() {
+    console.log('make btm')
+}
+function applyBtn() {
+    const inputElement = document.getElementById('inputId');
+    const inputString = inputElement.value;
+
+    const couponElement = document.getElementById('couponName');
+    const couponName = couponElement.innerText;
+
+    if (inputString === couponName) {
+        const totalPrice = getAmount('totalPrice');
+
+        const discountAmount = 0.2 * totalPrice;
+        const discount = document.getElementById('discountAmount');
+        discount.innerText = discountAmount.toFixed(2);
+
+
+        const totalPriceAmount = getAmount('totalPrice');
+        // const discountPrice = document.getElementById('discountAmount');
+        const discountPrice = getAmount('discountAmount');
+        const totalAmount = totalPriceAmount - discountPrice;
+
+        const totalTaka = document.getElementById('totalAmount');
+        totalTaka.innerText = totalAmount.toFixed(2);
+
+    }
+    else {
+        console.log(false);
+    }
+
+}
+
+
